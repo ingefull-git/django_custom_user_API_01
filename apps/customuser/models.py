fa4e552fa5afac6bb15c3763e4b8f6327607b05b
rulo <raul.sosa@globant.com>
2021-10-05 22:01:35 -0300

    start-commit

---
+++ apps/customuser/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+
+class CustomUserManager():
+    
+    pass
+
+
+class CustomUser(models.Model):
+
+    pass
