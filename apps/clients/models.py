9fc938f8f8853a7fff090428179c3be3c8e8cc87
rulo <rsosa.ingefull@gmail.com>
2021-10-24 20:53:46 -0300

    add: client Viewset, GenerigViewset and Routers

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -11,7 +11,7 @@ class Client(models.Model):
     fname       = models.CharField(verbose_name="first name", max_length=100, blank=True, null=True)
     lname       = models.CharField(verbose_name="last name", max_length=100, blank=False, null=False)
     email       = models.EmailField(verbose_name="email", blank=False, null=False)
-    status      = models.CharField(max_length=50, choices=Status.choices)
+    status      = models.CharField(max_length=50, choices=Status.choices, default=Status.OK)
     created     = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
 
--- apps/restapi/clients/urls.py
+++ apps/restapi/clients/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 
 from apps.restapi.clients import views
+from rest_framework.routers import DefaultRouter
 
+
+router = DefaultRouter()
+router.register('list-clients', views.ClientViewset, basename='clients-viewset')
+router.register('listgeneric-clients', views.ClientGenericViewset, basename='clients-generic-viewset')
 
 urlpatterns = [
     # path('list', views.client_list_api_view),
@@ -10,4 +15,5 @@ urlpatterns = [
     # path('<int:pk>/update', views.ClientRetrieveUpdateApiView.as_view()),
     path('<int:pk>/update', views.client_retieve_update_api_view),
     path('<int:pk>/detail', views.client_retrieve_update_delete_view),
+    path('viewset/', include(router.urls)),
 ]
